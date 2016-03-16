1f565f4f81964ed79d0c5415fe23757a8df99625
Dimdimovich <dimdimovich95@gmail.com>
2016-03-17 00:36:26 +0400

    Add created files
    Add three own modules

---
+++ ModuleOne.h
@@ -0,0 +1,110 @@
+typedef struct TheStruct{
+char f;   //field
+void *l;  //link
+} TheType;
+
+const TheSize = sizeof(TheType);
+
+int number(TheType *a){                         //Shows the count of cards
+int n=0;
+while (a&&++n)
+    a=a->l;
+return n;
+}
+void create(TheType *b){                        //Makes nice 52 card array
+char i,j;
+
+for (i=0;i<=12;i++)
+    for (j=0;j<=3;j++){
+       b->f=i*10+j;
+       if(13*j+i<51){
+          b->l=malloc(TheSize);
+          (b=b->l)->l=NULL;
+       }
+    }
+}
+void crempty(TheType *a){                       //Makes pure 52 cardss array
+char i;
+for (i=0;i<51;i++){
+    a->f=0;
+    a=(a->l=malloc(TheSize));
+    }
+a->f=0;
+a->l=NULL;
+}
+void del(TheType *a){                           //FREEs tha array
+if(a->l)
+    del(a->l);
+free(a);
+}
+
+void delLast(TheType *a){//DTima is programmer:) //FREEs the last element
+TheType *b;
+b=a->l;
+if(b->l)
+    delLast(b);
+else{
+    free(b);
+    a->l=NULL;
+}
+}
+
+char readF(TheType *a, char pos){               //Shows the element from 'a' on 'pos'
+int i;
+for(i=0;i<pos;i++)
+    a=a->l;
+return a->f;
+}
+void writeF(char dost, TheType *a, char pos){   //Puts card 'dost' in 'a' on 'pos' place
+int j;
+for(j=0;j<pos;j++)
+if ((a->l)!=NULL)  //Dima is programmer:) GUT PROGGER
+    a=a->l;
+else {
+    a=(a->l=malloc(TheSize));
+    a->f=255;
+    a->l=NULL;
+}
+a->f=dost;
+}
+
+void vivod(TheType *a){                         //Prints array
+while(a){
+  printf("%6d%2d",a->f/10,a->f%10);                        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  a=a->l;
+}
+printf("\n");
+}
+TheType* H(TheType *a){                         //Hash
+TheType *b;
+char i,j,h,m;
+//creating new array
+b=malloc(TheSize);
+b->l=NULL;
+crempty(b);
+
+//Computing non-zero variables
+h=m=0;
+while (!(h*m)){
+h = rand() % 52;
+m = rand() % 52;
+}
+//The Hash
+for (i=0;i<=51;i++){
+  j=(h*i+m) % 52;
+  while (readF(b,j)!=0)
+    j=(j+1) % 52;
+  writeF(readF(a,i),b,j);
+}
+
+//Deleting trash
+del(a);
+return b;
+}
+
+unsigned long power(unsigned long x, char n){
+if(n)
+return x*=power(x,n-1);
+else
+return 1;
+}
---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ModuleOne.h"
+#include "ModuleTwo.h"
+#include "ModuleThree.h"
+#include "Tims ModuleThree.h"
+
+int main(){
+TheType *a,*b,*c;
+char i,k;
+unsigned long j;
+
+srand(time(NULL)); //обновление таблички рандомных значений
+
+a=malloc(TheSize);
+a->l=NULL;
+
+create(a);
+
+a=H(H(H(a)));
+
+printf("\nAfter \"H\"\n");
+//vivod(a);
+
+printf("\n");
+
+b=seven(a,i);
+c=b;
+       c->f=10;
+(c=c->l)->f=20;
+(c=c->l)->f=33;
+(c=c->l)->f=41;
+(c=c->l)->f=50;
+(c=c->l)->f=83;
+(c=c->l)->f=13;
+
+ShowMeComb(street(b));
+vivod(b);
+
+
+
+del(a);
+return 0;
+}
